5ff4b7dc2b1281313538fd5530972bf243355910
Pranav <77088825+pranavjha21@users.noreply.github.com>
2021-01-14 12:44:27 +0530

    Update string_generator.py

--- string_generator.py
+++ string_generator.py
@@ -1,22 +1,13 @@
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
-print ("")
-print ("")
-print("""processing.......""")
 
-API_KEY = '(2327886)'
-API_HASH = "(b27fc6b63c22a6917438a57bc616075c)"
-while True:
-  try:
-   with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
-      print("")
-      session = client.session.save()
-      client.send_message("me", f"Here is your TELEGRAM STRING SESSION\n(tap to copy)👇 \n\n {session}")
-      print("You telegram String session successfully stored in your telegram, please check your Telegram Saved Messages ")
-      print(" Don't share it with anyone.. Regards.. KTech")
-  except:
-   print ("")
-   print ("Wrong phone number \n make sure its with correct  country code")
-   print ("")
-   continue
-  break
+print(
+    """Please go-to my.telegram.org
+Login using your Telegram account
+Click on API Development Tools
+Create a new application, by entering the required details"""
+)
+APP_ID = int(input("Enter APP ID here: "))
+API_HASH = input("Enter API HASH here: ")
+with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
+    print(client.session.save())
