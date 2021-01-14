07cfbe42d4026241c4cececf8b2792357478f3d0
Pranav <77088825+pranavjha21@users.noreply.github.com>
2021-01-14 13:01:20 +0530

    Update string_generator.py

--- string_generator.py
+++ string_generator.py
@@ -7,7 +7,7 @@ Login using your Telegram account
 Click on API Development Tools
 Create a new application, by entering the required details"""
 )
-APP_ID = int(input("Enter APP ID here: "))
-API_HASH = input("Enter API HASH here: ")
-with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
+APP_ID = int(input("2353687"))
+API_HASH = input("375fb45eb1ba90f2b1cbf08a5cc06e02")
+with TelegramClient(StringSession(1AZWarzYBu1z_1cYmyBBCY0n4gVTdVLEpTY_1gnGOwh6Vk8T40iCYaQz6Bfrq6b9oCGYt2ZWGiONuhwyWRkxNE1AcT7SAlol7U-yR7dpXEMIJB-cFz-cGyijYP4a3Eg-PXH_PMQpDa4MRLdHiv2XPsjz6_kaGybJmhio92y9b731UYB6Hh1Yc5NNhyLbXgymtA-lh5PK1QJo9SQgS3xgiSMdq1Th1RMltPYJn_XtmuinxdP13RjVQRkdK4yh-n1TMEImYkq5njB3EZOV6tvwQPmU6reg7yfASg3EZlQpFV_QFyULhiBm7CQIDD9Nma-lZ8aoF94qbKGWIaDoCnPN31sXtaH8d2Ks=), APP_ID, API_HASH) as client:
     print(client.session.save())
