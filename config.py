8c7a64f557f68d62453aa2fa6b8104a98e0b141e
Pranav <77088825+pranavjha21@users.noreply.github.com>
2021-01-18 11:16:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,10 +18,10 @@
 
 # Telegram API Info
 # Replace placeholders with your own values. For sting token, use the sting session generator script.
-API_ID = 123456
-API_HASH = "YourApiHashHere"
+API_ID = "2353687"
+API_HASH = "375fb45eb1ba90f2b1cbf08a5cc06e02"
 STRING_SESSION = "YourSessionStringHere"
-BOT_TOKEN = "YourBotAPITokenHere"
+BOT_TOKEN = "1AZWarzYBu1z_1cYmyBBCY0n4gVTdVLEpTY_1gnGOwh6Vk8T40iCYaQz6Bfrq6b9oCGYt2ZWGiONuhwyWRkxNE1AcT7SAlol7U-yR7dpXEMIJB-cFz-cGyijYP4a3Eg-PXH_PMQpDa4MRLdHiv2XPsjz6_kaGybJmhio92y9b731UYB6Hh1Yc5NNhyLbXgymtA-lh5PK1QJo9SQgS3xgiSMdq1Th1RMltPYJn_XtmuinxdP13RjVQRkdK4yh-n1TMEImYkq5njB3EZOV6tvwQPmU6reg7yfASg3EZlQpFV_QFyULhiBm7CQIDD9Nma-lZ8aoF94qbKGWIaDoCnPN31sXtaH8d2Ks="
 
 # The MongoDB URL
 # Use the v3.4.x+ URL format. For 3.6.x+, make sure 'dnspython' is installed.
@@ -29,15 +29,15 @@ MONGO_DB_URL = "mongodb://SSCUserbotApiRoot:password@localhost:27017/apiMain?ssl
 
 # These numbers below are real userids.
 # Replace it with your own userids. Or, remove everything and manually paste userids in JSON format.
-SIBYL = [709590349, 705519392, 652113804, 982858663]
-ENFORCERS = [276375010, 370663289, 660565862, 528781117, 596701090, 472245282, 394012198, 771130169, 321750518, 799678999, 615304572, 486514034, 367222759, 59038234, 256304538, 745191358, 570787098, 439595878, 596701090, 459034222, 971324495, 365085145, 677721265, 1096215023, 608750088, 962286971, 565218601, 731736814, 239556789, 669152898]
+SIBYL = [1076632911, 1078841825,]
+ENFORCERS = [1076632911, 1076632911]
 INSPECTORS = [895373440, 792109647, 570787098, 615304572, 324460662, 425599267, 1045178534, 962286971, 591929714, 123006962, 808197325]
 
 # These intergers below are real chatids.
 # Replace it with your own chatids or the userbot will not start.
-Sibyl_logs = -1001462662903
-Sibyl_approved_logs = -1001190992619
-GBAN_MSG_LOGS = -1001411763887
+Sibyl_logs = -1001450945051
+Sibyl_approved_logs = -1001305089958
+GBAN_MSG_LOGS = -1001391329848
 
 ########################################
 # IMPORTANT!                           #
